be34a6b175fad972af0eb630f7a278a0a1203ebc
Momchil Atanasov <momchil.dev@gmail.com>
2023-01-23 22:28:00 +0200

    Recreate more shaders with templates

---
+++ game/shaders/pbr_geometry.frag.glsl
@@ -0,0 +1,46 @@
+/*template "version.glsl"*/
+
+layout(location = 0) out vec4 fbColor0Out;
+layout(location = 1) out vec4 fbColor1Out;
+
+/*if .UseAlbedoTexture*/
+layout(binding = 0) uniform sampler2D albedoTwoDTextureIn;
+/*end*/
+
+// TODO: Export to template
+layout (binding = 2, std140) uniform Material
+{
+	vec4 albedoColorIn;
+	float alphaThresholdIn;
+	float normalScaleIn;
+	float metallicIn;
+	float roughnessIn;
+};
+
+smooth in vec3 normalInOut;
+/*if .UseTexturing*/
+smooth in vec2 texCoordInOut;
+/*end*/
+/*if .UseVertexColoring*/
+smooth in vec4 colorInOut;
+/*end*/
+
+void main()
+{
+	/*if .UseAlbedoTexture*/
+	vec4 color = texture(albedoTwoDTextureIn, texCoordInOut);
+	/*else if .UseVertexColoring*/
+	vec4 color = colorInOut;
+	/*else*/
+	vec4 color = albedoColorIn;
+	/*end*/
+
+  /*if .UseAlphaTest*/
+	if (color.a < alphaThresholdIn) {
+		discard;
+	}
+	/*end*/
+
+	fbColor0Out = vec4(color.xyz, metallicIn);
+	fbColor1Out = vec4(normalize(normalInOut), roughnessIn);
+}
