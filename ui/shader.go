db3f3237d0a15c2b347d72c936151012481b655c
Momchil Atanasov <momchil.dev@gmail.com>
2023-03-01 00:24:28 +0200

    Relocate shaders

--- ui/shader.go
+++ ui/shader.go
@@ -1,53 +1,8 @@
 package ui
 
 import (
-	"bytes"
-	"embed"
-	"fmt"
-	"text/template"
-
+	"github.com/mokiat/lacking-gl/shader"
 	"github.com/mokiat/lacking/ui"
-)
-
-//go:embed shaders/*
-var sources embed.FS
-
-var rootTemplate = template.Must(template.
-	New("root").
-	Delims("/*", "*/").
-	ParseFS(sources, "shaders/*.glsl"),
-)
-
-func find(name string) *template.Template {
-	result := rootTemplate.Lookup(name)
-	if result == nil {
-		panic(fmt.Errorf("template %q not found", name))
-	}
-	return result
-}
-
-var buffer = new(bytes.Buffer)
-
-func runTemplate(tmpl *template.Template, data any) string {
-	buffer.Reset()
-	if err := tmpl.Execute(buffer, data); err != nil {
-		panic(fmt.Errorf("template exec error: %w", err))
-	}
-	return buffer.String()
-}
-
-var (
-	tmplShadedShapeVertexShader   = find("shaded_shape.vert.glsl")
-	tmplShadedShapeFragmentShader = find("shaded_shape.frag.glsl")
-
-	tmplBlankShapeVertexShader   = find("blank_shape.vert.glsl")
-	tmplBlankShapeFragmentShader = find("blank_shape.frag.glsl")
-
-	tmplContourVertexShader   = find("contour.vert.glsl")
-	tmplContourFragmentShader = find("contour.frag.glsl")
-
-	tmplTextVertexShader   = find("text.vert.glsl")
-	tmplTextFragmentShader = find("text.frag.glsl")
 )
 
 func NewShaderCollection() ui.ShaderCollection {
@@ -61,28 +16,28 @@ func NewShaderCollection() ui.ShaderCollection {
 
 func newShadedShapeShaderSet() ui.ShaderSet {
 	return ui.ShaderSet{
-		VertexShader:   runTemplate(tmplShadedShapeVertexShader, struct{}{}),
-		FragmentShader: runTemplate(tmplShadedShapeFragmentShader, struct{}{}),
+		VertexShader:   shader.RunTemplate("shaded_shape.vert.glsl", struct{}{}),
+		FragmentShader: shader.RunTemplate("shaded_shape.frag.glsl", struct{}{}),
 	}
 }
 
 func newBlankShapeShaderSet() ui.ShaderSet {
 	return ui.ShaderSet{
-		VertexShader:   runTemplate(tmplBlankShapeVertexShader, struct{}{}),
-		FragmentShader: runTemplate(tmplBlankShapeFragmentShader, struct{}{}),
+		VertexShader:   shader.RunTemplate("blank_shape.vert.glsl", struct{}{}),
+		FragmentShader: shader.RunTemplate("blank_shape.frag.glsl", struct{}{}),
 	}
 }
 
 func newContourShaderSet() ui.ShaderSet {
 	return ui.ShaderSet{
-		VertexShader:   runTemplate(tmplContourVertexShader, struct{}{}),
-		FragmentShader: runTemplate(tmplContourFragmentShader, struct{}{}),
+		VertexShader:   shader.RunTemplate("contour.vert.glsl", struct{}{}),
+		FragmentShader: shader.RunTemplate("contour.frag.glsl", struct{}{}),
 	}
 }
 
 func newTextShaderSet() ui.ShaderSet {
 	return ui.ShaderSet{
-		VertexShader:   runTemplate(tmplTextVertexShader, struct{}{}),
-		FragmentShader: runTemplate(tmplTextFragmentShader, struct{}{}),
+		VertexShader:   shader.RunTemplate("text.vert.glsl", struct{}{}),
+		FragmentShader: shader.RunTemplate("text.frag.glsl", struct{}{}),
 	}
 }
